406ce4fd897c2f5ddfca652e8789f13536cdfe39
wwowwww <98000579+wwowwww@users.noreply.github.com>
2024-04-22 13:40:27 +0900

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
+import telegram
 from dotenv import load_dotenv
 from pydantic import BaseSettings
 
@@ -10,3 +11,17 @@ class Settings(BaseSettings):
     redis_host: str = os.getenv('REDIS_HOST')
     redis_port: int = os.getenv('REDIS_PORT')
     redis_db: int = os.getenv('REDIS_DB')
+    
+    telegram_token: str = os.getenv('TELEGRAM_TOKEN')
+    telegram_chat_id: str = os.getenv('TELEGRAM_CHAT_ID')
+
+    etherscan_api_key: str = os.getenv('ETHERSCAN_API_KEY')
+    address: str = os.getenv('ADDRESS')
+
+
+def send_telegram_message(message: str):
+    bot = telegram.Bot(token=Settings().telegram_token)
+    bot.sendMessage(
+        chat_id=Settings().telegram_chat_id, 
+        text=message
+    )
